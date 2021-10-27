f9dfd563ec6ee23739fafde6880460aeba50184a
Francis <francisfrancois9189@gmail.com>
2021-10-27 16:00:49 +0200

    updateDelete

--- addCharacter.js
+++ addCharacter.js
@@ -1,19 +1,33 @@
-let currentBase64;
-let base64Split;
-const imageToBase64 = (element) => {
-  const file = event.target.files[0];
-  const reader = new FileReader();
-  reader.onloadend = () => {
-    currentBase64 = reader.result;
-    base64Split = currentBase64.split(",")[1];  
-};
-reader.readAsDataURL(file);
-};
+let imgInput = document.getElementById('file-ip-1');
+imgInput.addEventListener('change', function (e) {
+    if (e.target.files) {
+        let imageFile = e.target.files[0];
+        let reader = new FileReader();
+        reader.onload = function (e) {
+            let img = document.createElement("img");
+            img.onload = function (event) {
+                // Dynamically create a canvas element
+                let canvas = document.createElement("canvas");
+                canvas.width = 100;
+                canvas.height = 100;
 
+                // var canvas = document.getElementById("canvas");
+                let ctx = canvas.getContext("2d");
+                // Actual resizing
+                ctx.drawImage(img, 0, 0, 100, 100);
 
-
-
-
+                // Show resized image in preview element
+                let dataurl = canvas.toDataURL(imageFile.type, 1);
+                document.getElementById("image").src = dataurl;
+                
+            }
+            img.src = e.target.result;
+            document.getElementById("image").style.display = 'block';
+        }
+        reader.readAsDataURL(imageFile);
+        
+    }
+});
 
 
 const src = "https://character-database.becode.xyz/characters";
@@ -23,6 +37,7 @@ const src = "https://character-database.becode.xyz/characters";
       const name = document.querySelector("#new-character-name").value;
       const shortDescription = document.querySelector("#shortDescription").value;
       const description = document.querySelector("#description").value; 
+      const image = document.querySelector("#image").src.slice(23, document.querySelector("#image").src.lenght);
       
      
     
@@ -37,6 +52,8 @@ const src = "https://character-database.becode.xyz/characters";
                   shortDescription: shortDescription,
                   description: description,
                   image: image
+                  
+                 
 
                  
               })
@@ -46,58 +63,3 @@ const src = "https://character-database.becode.xyz/characters";
 });
 
 
-/*
-
-const regeneratorRuntime = require("regenerator-runtime/runtime");
-
-const recievedImage = document.querySelector("#new-image");
-const recievedName = document.getElementById("new-name");
-const recievedShortDescription = document.querySelector("#shortDescription");
-const recievedDescription = document.querySelector("#markdown");
-const imageUpload = document.getElementById("user-image");
-const beCodeUrl = https://character-database.becode.xyz/characters%60;
-
-let currentBase64;
-let base64Split;
-const imageToBase64 = (element) => {
-  const file = event.target.files[0];
-  const reader = new FileReader();
-  reader.onloadend = () => {
-    currentBase64 = reader.result;
-    // console.log(currentBase64)
-    // imageDisplay.src = currentBase64;
-    // tempoImage.src = currentBase64;
-    // tempoContainer.append(tempoImage);
-    // actionDiv.append(tempoContainer);
-    base64Split = currentBase64.split(",")[1]; //picking only the base 64 without the haders but this seems to be unnessesary it works well with out split
-  };
-  reader.readAsDataURL(file);
-};
-
-let newCharacterObject = {};
-
-document.querySelector("#submit").addEventListener("click", async () => {
-  event.preventDefault();
-
-  newCharacterObject.name = recievedName.value;
-  newCharacterObject.shortDescription = recievedShortDescription.value;
-  newCharacterObject.description = recievedDescription.value;
-  newCharacterObject.image = base64Split; //currentBase64;
-
-  fetch(beCodeUrl, {
-    method: "post",
-    body: JSON.stringify(newCharacterObject),
-    headers: {
-      "content-type": "application/json; charset=UTF-8",
-    },
-  });
-
-  console.log(newCharacterObject);
-  // }
-
-  console.log("i am the submit button");
-});
-
-imageUpload.addEventListener("change", imageToBase64);`
-
-Envoyer un message à @Fradven
--- deleteCharacter.js
+++ deleteCharacter.js
@@ -1,18 +1,31 @@
+function myConfirm() {
+  let result = confirm("Want to delete?");
+  if (result==true) {
+   return true;
+  } else {
+   return false;
+  }
+}
+
 (() => {
     const input = document.querySelector("#tpl-card")
 
-    document.querySelector('#buttonDelete').addEventListener("click", async () => {
+    document.querySelector('.btn-danger').addEventListener("click", async () => {
+        console.log("hello");
+        /*
         try {
             const response = await fetch("http://character-database.becode.xyz/characters"+input.value, {
                 method: 'DELETE'
+                
             });
+           
             if(!response.ok) throw new Error(response.statusText)
             const getAll = await fetch('http://character-database.becode.xyz/characters')
             const data = await getAll.json()
             console.log(data)
         } catch (err) {
             alert(err)
-        }
+        } */
     });
 })();
 
